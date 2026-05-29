07428c135c044a5e9fef39e0a54ad631db94f532
Summer_Assignment_25110CN012 <officialamanyadav2025@gmail.com>
2026-05-29 13:15:10 +0530

    Added Day2 C++ programs

---
+++ code1.cpp
@@ -0,0 +1,23 @@
+// Write a program to Find sum of digits of a
+// number
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num, sum = 0, digit;
+
+  cout << "Enter a number: ";
+  cin >> num;
+
+  while (num != 0) {
+    digit = num % 10;        
+    sum = sum + digit;  
+    num = num / 10;  
+    }
+
+  cout << "Sum of digits = " << sum;
+
+  
+  return 0;
+}
