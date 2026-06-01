6ea3456cd3c8349feb234777196f1379e3be158c
Summer_Assignment_25110CN012 <officialamanyadav2025@gmail.com>
2026-06-01 12:19:20 +0530

    Added Day5 Assignment

---
+++ Day5/code4.cpp
@@ -0,0 +1,27 @@
+// Write a program to Find largest prime factor. 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  long long n;
+  cout << "Enter a number: ";
+  cin >> n;
+  long long largestPrime = -1;
+  while (n % 2 == 0) {
+    largestPrime = 2;
+    n /= 2;
+  }
+
+  for (long long i = 3; i * i <= n; i += 2) {
+    while (n % i == 0) {
+      largestPrime = i;
+      n /= i;
+    }
+  }
+  if (n > 2)
+    largestPrime = n;
+  cout << "Largest Prime Factor = " << largestPrime;
+
+  return 0;
+}
