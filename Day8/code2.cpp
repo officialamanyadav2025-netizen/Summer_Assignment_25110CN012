a38480d6a963456b436b7e1f09bf879a0a9221d2
Summer_Assignment_25110CN012 <officialamanyadav2025@gmail.com>
2026-06-04 16:22:14 +0530

    Added Day8 Assignment

---
+++ Day8/code2.cpp
@@ -0,0 +1,21 @@
+// Write a program to Print number triangle.
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int rows = 5;
+
+  for (int i = 1; i <= rows; i++) {
+    for (int j = 1; j <= i; j++) {
+      cout << j;
+    }
+  cout << endl;
+  }
+  return 0;
+}
---
+++ Day8/code4.cpp
@@ -0,0 +1,23 @@
+// Write a program to Print repeated-number
+// pattern.
+// 1
+// 22
+// 333
+// 4444
+// 55555
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int rows = 5;
+
+  for (int i = 1; i <= rows; i++) {
+    for (int j = 1; j <= i; j++) {
+      cout << i;
+    }
+  cout << endl;
+  }
+  
+  return 0;
+}
