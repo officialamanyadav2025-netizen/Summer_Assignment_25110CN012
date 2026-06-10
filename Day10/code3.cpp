0a159ddd94eecb47a8c2a49a5786686fe64a6709
Summer_Assignment_25110CN012 <officialamanyadav2025@gmail.com>
2026-06-10 18:42:12 +0530

    Fix Day10 folder upload

---
+++ Day10/code3.cpp
@@ -0,0 +1,20 @@
+// Write a program to Print number pyramid.
+// 1
+// 121
+// 12321
+// 1234321
+// 123454321
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n = 5;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+      cout << j;
+    }
+    cout << endl;
+  }
+  return 0;
+}
---
+++ Day10/code4.cpp
@@ -0,0 +1,26 @@
+// Write a program to Print character pyramid. Microsoft, Adobe, Paytm
+//    A
+//   ABA
+//  ABCBA
+// ABCDCBA
+//ABCDEDCBA
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n = 5;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n - i; j++) {
+      cout << " ";
+    }
+    for (char ch = 'A'; ch < 'A' + i; ch++) {
+      cout << ch;
+    }
+    for (char ch = 'A' + i - 2; ch >= 'A'; ch--) {
+      cout << ch;
+    }
+  cout << endl;
+  }
+  return 0;
+}
