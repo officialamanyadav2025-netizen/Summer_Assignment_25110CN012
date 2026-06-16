43ffa277abd4429ed91c47bbc63d454ef93b1b2c
Summer_Assignment_25110CN012 <officialamanyadav2025@gmail.com>
2026-06-16 10:42:14 +0530

    Added Day16 assignment

---
+++ Day16/code1.cpp
@@ -0,0 +1,30 @@
+// Write a program to Merge arrays.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n, m;
+  cout << "Enter size of first array: ";
+  cin >> n;
+  int a[n];
+  cout << "Enter elements of first array: ";
+  for(int i = 0; i < n; i++)
+    cin >> a[i];
+  cout << "Enter size of second array: ";
+  cin >> m;
+  int b[m];
+  cout << "Enter elements of second array: ";
+  for(int i = 0; i < m; i++)
+    cin >> b[i];
+  int c[n + m];
+  for(int i = 0; i < n; i++)
+    c[i] = a[i];
+  for(int i = 0; i < m; i++)
+    c[n + i] = b[i];
+  cout << "Merged Array: ";
+  for(int i = 0; i < n + m; i++)
+    cout << c[i] << " ";
+  
+    return 0;
+}
---
+++ Day16/code2.cpp
@@ -0,0 +1,31 @@
+// Write a program to Union of arrays. 
+
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+  int n, m;
+  cout << "Enter size of first array: ";
+  cin >> n;
+  int a[n];
+  cout << "Enter elements of first array: ";
+  for(int i = 0; i < n; i++)
+    cin >> a[i];
+  cout << "Enter size of second array: ";
+  cin >> m;
+  int b[m];
+  cout << "Enter elements of second array: ";
+  for(int i = 0; i < m; i++)
+    cin >> b[i];
+  set<int> s;
+  for(int i = 0; i < n; i++)
+    s.insert(a[i]);
+  for(int i = 0; i < m; i++)
+    s.insert(b[i]);
+  cout << "Union: ";
+  for(int x : s)
+    cout << x << " ";
+  
+    return 0;
+}
