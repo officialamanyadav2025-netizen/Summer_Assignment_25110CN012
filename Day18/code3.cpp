36d6c3f08e27cd19cc365a9a22c4278ab57577ef
Summer_Assignment_25110CN012 <officialamanyadav2025@gmail.com>
2026-06-18 21:03:51 +0530

    Added Day18 assignment

---
+++ Day18/code3.cpp
@@ -0,0 +1,47 @@
+// Write a program to Binary search.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, key;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter sorted elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    int low = 0;
+    int high = n - 1;
+    bool found = false;
+
+    while(low <= high) {
+        int mid = (low + high) / 2;
+
+        if(arr[mid] == key) {
+            cout << "Element found at index " << mid;
+            found = true;
+            break;
+        }
+        else if(arr[mid] < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    if(!found) {
+        cout << "Element not found";
+    }
+
+    return 0;
+}
